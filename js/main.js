2b059d06b9f0a55affe968bd6e3fe686761c883f
pawanpatil94 <pawanpatil94@gmail.com>
2017-04-05 23:21:08 -0700

    Working

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 	function loadQuote(){
 		var color = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
 	var temp = Math.floor(Math.random() * (5));
-	$('body, #changeQuote').fadeIn().css('background-color', color[temp]);
+	$('body, #changeQuote').css('background-color', color[temp]);
 		$.ajax({
 			url: 'http://api.forismatic.com/api/1.0/',
 			data: {
